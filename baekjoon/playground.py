4b96745a6b651e9248b097e465cde42a20e549ce
ming2tofu33 <ming2tofu33@gmail.com>
2025-09-21 00:03:48 +0900

    p;

--- baekjoon/playground.py
+++ baekjoon/playground.py
@@ -1,8 +1,26 @@
 import sys
 
-t = int(sys.stdin.readline())
+n = int(sys.stdin.readline())
 
-for _ in range(t):
-    _ = int(sys.stdin.readline())
-    n = list(map(int, sys.stdin.readline().split()))
-    print(min(n), max(n))
+l = list(map(int, sys.stdin.read().split()))
+l.sort()
+
+s = set(l)
+
+c = set()
+
+for i in s:
+    tmp = l.count(i)
+    if tmp > 1:
+        c = {i}
+    elif tmp == max(c):
+        c.add(i)
+
+cnt = sorted(list(c))
+
+print(n, l, '==del==')
+
+print(round(sum(l) / n))
+print(l[len(l) // 2] if len(l) % 2 else (l[len(l) // 2 - 1] + l[len(l) // 2]) / 2)
+print(cnt[-2] if len(c) > 1 else cnt[0])
+print(max(l) - min(l))
