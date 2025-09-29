5340f9d859b90460783584e9d39dee63006086e5
ming2tofu33 <ming2tofu33@gmail.com>
2025-09-29 14:44:35 +0900

    p

--- baekjoon/playground.py
+++ baekjoon/playground.py
@@ -1,26 +1,11 @@
 import sys
 
-n = int(sys.stdin.readline())
+for line in sys.stdin:
+    line = line.rstrip('\n')
+    if line == '#': 
+        break
 
-l = list(map(int, sys.stdin.read().split()))
-l.sort()
-
-s = set(l)
-
-c = set()
-
-for i in s:
-    tmp = l.count(i)
-    if tmp > 1:
-        c = {i}
-    elif tmp == max(c):
-        c.add(i)
-
-cnt = sorted(list(c))
-
-print(n, l, '==del==')
-
-print(round(sum(l) / n))
-print(l[len(l) // 2] if len(l) % 2 else (l[len(l) // 2 - 1] + l[len(l) // 2]) / 2)
-print(cnt[-2] if len(c) > 1 else cnt[0])
-print(max(l) - min(l))
+    letter, sentence = line.split(' ', 1) 
+    letter = letter.lower()
+    cnt = sum(1 for ch in sentence if ch.lower() == letter)
+    print(letter, cnt)
