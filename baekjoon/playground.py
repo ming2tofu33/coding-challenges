80dcc3e90e0b79642f59619c69bc550425a7dff7
ming2tofu33 <ming2tofu33@gmail.com>
2025-10-11 23:42:08 +0900

    p

--- baekjoon/playground.py
+++ baekjoon/playground.py
@@ -1,7 +1,9 @@
+# 11651 "좌표 정렬하기"
+
 import sys
 
 n = int(sys.stdin.readline())
-pts = [tuple(map(int, sys.stdin.readline().split())) for _ in range(n)]
-pts.sort()
+pairs = [tuple(reversed(tuple(map(int, sys.stdin.readline().split())))) for _ in range(n)]  # (y,x)
+pairs.sort()
 
-sys.stdout.write('\n'.join(f'{x} {y}' for x, y in pts))
+sys.stdout.write('\n'.join(f'{x} {y}' for y, x in pairs))
