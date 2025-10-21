28c964a864fe4d467449c94c0b87989fd7413e40
ming2tofu33 <ming2tofu33@gmail.com>
2025-10-21 21:53:39 +0900

    p

--- baekjoon/playground.py
+++ baekjoon/playground.py
@@ -5,11 +5,3 @@ import sys
 n, m = map(int, sys.stdin.readline().split())
 nums = list(map(int, sys.stdin.readline().split()))
 
-acc_nums = [0]
-
-for num in nums:
-    acc_nums.append(acc_nums[-1] + num)
-    
-for _ in range(m):
-    i, j = map(int, sys.stdin.readline().split())
-    sys.stdout.write(f"{acc_nums[j] - acc_nums[i - 1]} \n")
