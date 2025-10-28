195fbd1892bda7a24af5325caf58f07464402ea3
ming2tofu33 <ming2tofu33@gmail.com>
2025-10-28 18:53:49 +0900

    p

--- baekjoon/playground.py
+++ baekjoon/playground.py
@@ -3,43 +3,31 @@ from collections import deque
 input = sys.stdin.readline
 
 M, N = map(int, input().split())
-box = []
+box = [list(map(int, input().split())) for _ in range(N)]
 
-for i in range(N):
-    box.append(list(map(int, input().split())))
-
-ripe = deque()
-days = 0
-dr = [-1, -1, 0, 0]
+q = deque()
+dr = [-1, 1, 0, 0]
 dc = [0, 0, -1, 1]
 
-def valid(x, y):
-    v = 0
-    for i in range(4):
-        nr = x + dr[i]
-        nc = y + dc[i]
-        if nr < 0 or nr >= M or nc < 0 or nc >= N or box[nc][nr] == -1:
-            v += 1
-    if v != 4:
-        return True
-    return False
-        
-def ripen(l):
-    pass
-    
-for row in range(N):
-    for col in range(M):
-        if not valid(col, row):
-            days = -1
-            break
-        if box[col][row] == 1:
-            ripe.append((col, row))
-        
+for r in range(N):
+    for c in range(M):
+        if box[r][c] == 1:
+            q.append((r, c))
 
-# while ripe:
+while q:
+    r, c = q.popleft()
+    for k in range(4):
+        nr, nc = r + dr[k], c + dc[k]
+        if 0 <= nr < N and 0 <= nc < M and box[nr][nc] == 0:
+            box[nr][nc] = box[r][c] + 1
+            q.append((nr, nc))
 
+ans = 0
+for r in range(N):
+    for c in range(M):
+        if box[r][c] == 0:
+            print(-1)
+            sys.exit(0)
+        ans = max(ans, box[r][c])
 
-
-#########
-
-print(box)
+print(ans - 1)
