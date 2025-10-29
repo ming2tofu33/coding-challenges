3d4ae98d7012eb85fed8b2c6a8fc454ec374362c
ming2tofu33 <ming2tofu33@gmail.com>
2025-10-29 19:02:57 +0900

    p

--- baekjoon/playground.py
+++ baekjoon/playground.py
@@ -2,32 +2,44 @@ import sys
 from collections import deque
 input = sys.stdin.readline
 
-M, N = map(int, input().split())
-box = [list(map(int, input().split())) for _ in range(N)]
+N, M, V = map(int, input().split())
 
-q = deque()
-dr = [-1, 1, 0, 0]
-dc = [0, 0, -1, 1]
+adj = [[] for _ in range(N + 1)]
+for _ in range(M):
+    a, b = map(int, input().split())
+    adj[a].append(b)
+    adj[b].append(a)
 
-for r in range(N):
-    for c in range(M):
-        if box[r][c] == 1:
-            q.append((r, c))
+for i in range(1, N + 1):
+    adj[i].sort()
+
+dfs_order = []
+visited = [False] * (N + 1)
+stack = [V]
+
+while stack:
+    node = stack.pop()
+    if visited[node]:
+        continue
+    visited[node] = True
+    dfs_order.append(node)
+    for nxt in reversed(adj[node]):
+        if not visited[nxt]:
+            stack.append(nxt)
+
+print(*dfs_order)
+
+bfs_order = []
+visited = [False] * (N + 1)
+q = deque([V])
+visited[V] = True
 
 while q:
-    r, c = q.popleft()
-    for k in range(4):
-        nr, nc = r + dr[k], c + dc[k]
-        if 0 <= nr < N and 0 <= nc < M and box[nr][nc] == 0:
-            box[nr][nc] = box[r][c] + 1
-            q.append((nr, nc))
+    node = q.popleft()
+    bfs_order.append(node)
+    for nxt in adj[node]:
+        if not visited[nxt]:
+            visited[nxt] = True 
+            q.append(nxt)
 
-ans = 0
-for r in range(N):
-    for c in range(M):
-        if box[r][c] == 0:
-            print(-1)
-            sys.exit(0)
-        ans = max(ans, box[r][c])
-
-print(ans - 1)
+print(*bfs_order)
