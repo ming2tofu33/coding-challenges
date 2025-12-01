b614423a5f4c4819b01adf3d192fb757f86e4d6a
mign2tofu33 <ming2tofu33@gmail.com>
2025-12-01 17:31:14 +0900

    485

--- baekjoon/playground.py
+++ baekjoon/playground.py
@@ -1,26 +1,16 @@
-# 9946 "단어 퍼즐"
+# 10989 "수 정렬하기 3"
 
-from collections import Counter
 import sys
-input = sys.stdin.readline
 
-def game(before, after):
-    before_count = Counter(before)
-    after_count = Counter(after)
-    
-    if before_count == after_count:
-        return "same"
-    else:
-        return "different"
+n = int(sys.stdin.readline())
 
-num = 1
+cnt = [0] * 10001
 
-while True:
-    before = input().strip()
-    after = input().strip()
-    
-    if before == "END" and after == "END":
-        break
-    
-    print(f'Case {num}: {game(before, after)}')
-    num += 1
+for _ in range(n):
+    a = int(sys.stdin.readline())
+    cnt[a] += 1
+
+for i in range(1, 10001):
+    c = cnt[i]
+    if c:
+        sys.stdout.write((str(i) + '\n') * c)
