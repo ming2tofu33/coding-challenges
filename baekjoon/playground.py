22f7ad024787d7e740dd022ff820bc583cadd8f6
mign2tofu33 <ming2tofu33@gmail.com>
2025-12-26 23:43:40 +0900

    p

--- baekjoon/playground.py
+++ baekjoon/playground.py
@@ -1,25 +1,21 @@
-# 1406 "에디터"
+# 10820 "문자열 분석"
 
-from collections import deque
 import sys
-input = sys.stdin.readline
 
-left = deque(input().rstrip())
-right = deque()
-n = int(input())
+lines = sys.stdin.read().splitlines()
 
-for _ in range(n):
-    cmd = input().rstrip()
-    if cmd[0] == 'P':
-        left.append(cmd[2])
-    elif cmd[0] == 'L':
-        if left:
-            right.appendleft(left.pop())
-    elif cmd[0] == 'D':
-        if right:
-            left.append(right.popleft())
-    elif cmd[0] == 'B':
-        if left:
-            left.pop()
-
-print(''.join(left) + ''.join(right))
+for line in lines:
+    lowercase = 0
+    uppercase = 0
+    numeric = 0
+    space = 0
+    for ch in line:
+        if ch.islower():
+            lowercase +=1
+        elif ch.isupper():
+            uppercase += 1
+        elif ch.isnumeric():
+            numeric += 1
+        elif ch == ' ':
+            space += 1
+    print(lowercase, uppercase, numeric, space)
